2abfb53e91ee0657ce2f51ad77d500a062a6ab83
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-08 20:23:09 +0200

    First commit

---
+++ src/main/java/main/main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

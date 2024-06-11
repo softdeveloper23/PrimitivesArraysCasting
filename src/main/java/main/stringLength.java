b36a98508fb889d71f33d53a500592e71fdce423
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-11 20:14:02 +0200

    Added class stringLength and code

---
+++ src/main/java/main/stringLength.java
@@ -0,0 +1,10 @@
+package main;
+
+public class stringLength {
+    public static void main(String[] args) {
+        String text = "Java Programming";
+
+        int length = text.length();
+        System.out.println(length);
+    }
+}

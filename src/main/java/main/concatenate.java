d42e01564b35f6cdfe564b51248d2794bcdaf206
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-11 19:59:34 +0200

    Added concatenate class and code

---
+++ src/main/java/main/concatenate.java
@@ -0,0 +1,12 @@
+package main;
+
+public class concatenate {
+    public static void main(String[] args) {
+        String str1, str2;
+        str1 = "Hello, ";
+        str2 = "World!";
+
+        String result = str1 + str2;
+        System.out.println(result);
+    }
+}

e81eb504f971dbcbdbe93b0ab59ddf2681821ed2
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-11 20:28:22 +0200

    Added divide class and code

---
+++ src/main/java/main/divide.java
@@ -0,0 +1,11 @@
+package main;
+
+public class divide {
+    public static void main(String[] args) {
+        double x, y;
+        x = 7.5;
+        y = 2.3;
+        double result = x / y;
+        System.out.println("x / y = " + result);
+    }
+}

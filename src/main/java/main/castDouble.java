da644587392bfeda3ab748ac6759687d664afe52
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-11 20:30:35 +0200

    Added castDouble class and code

---
+++ src/main/java/main/castDouble.java
@@ -0,0 +1,9 @@
+package main;
+
+public class castDouble {
+    public static void main(String[] args) {
+        double number = 23.8;
+        int integer = (int) number;
+        System.out.println(integer);
+    }
+}

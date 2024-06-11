cfa02ead25739d3894e9e1deef9cd2ff8db92525
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-11 20:11:48 +0200

    Added multiply class and code

---
+++ src/main/java/main/multiply.java
@@ -0,0 +1,12 @@
+package main;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 3;
+
+        int result = a * b;
+        System.out.println("a * b = " + result);
+    }
+}

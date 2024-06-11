6a9e7ab94416ba77bb6f6a5d0dea80f3a3a206cb
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-11 20:20:19 +0200

    Added fruitsArray class and code

---
+++ src/main/java/main/fruitsArray.java
@@ -0,0 +1,8 @@
+package main;
+
+public class fruitsArray {
+    public static void main(String[] args) {
+        String[] fruits = {"apple", "orange", "banana", "apple", "orange", "banana"};
+        System.out.println(fruits[0]);
+    }
+}
